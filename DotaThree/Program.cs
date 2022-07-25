ba19b77866a3c2061e72c547c463858ff238418c
Maxkkzx <maxkkzx@gmail.com>
2022-07-26 01:30:40 +0300

    the beginning of the creation of a hero

--- DotaThree/Program.cs
+++ DotaThree/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿class Hero
+{
+    public static void Move()
+    {
+        Console.WriteLine("Герой идёт вперёд!");
+    }
+    public static void Attack()
+    {
+        Console.WriteLine("Герой нанёс удар");
+    }
+    public static int Random()
+    {
+        Random r = new();
+        int randomIndex = r.Next(0, 5);
+        return randomIndex;
+    }
+}
+class Invoker : Hero
+{
+    
+}
+
+
