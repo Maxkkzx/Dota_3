b137a8cc0a91ff531230dbeaafd1fa3b0b9bb377
Maxkkzx <maxkkzx@gmail.com>
2022-07-27 00:04:32 +0300

    Update the base class, attribute system and added two classes (hero)

--- DotaThree/Program.cs
+++ DotaThree/Program.cs
@@ -1,9 +1,8 @@
-﻿Pudge.Hook();
-class Hero
+﻿class Hero
 {
-    public int Strength = 20;
-    public int Agility = 15;
-    public int Intelligence = 15; 
+    public int Strength { get; set; }
+    public int Agility { get; set; }
+    public int Intelligence { get; set; }
     public static void Move()
     {
         Console.WriteLine("Герой идёт вперёд!");
@@ -21,6 +20,14 @@ class Hero
 }
 class Pudge : Hero
 {
+    public static void Atribute()
+    {
+        Pudge pudge = new Pudge();
+        pudge.Agility = 25;
+        pudge.Strength = 14;
+        pudge.Intelligence = 16;
+        Console.WriteLine($"Сила: {pudge.Strength}  Ловкость: {pudge.Agility}  Интеллект: {pudge.Intelligence}");
+    }
     public static void Hook()
     {
         Console.WriteLine("Pudge Бросил мясной крюк!");
@@ -35,11 +42,66 @@ class Pudge : Hero
     }
     public static void Root()
     {
-        Console.WriteLine("Ядовитое облако появилось вокруг Pudge");
+        Console.WriteLine("Ядовитое облако появилось вокруг Pudge!");
     }
     public static void Dismember()
     {
-        Console.WriteLine("Pudge начинает заживо пожирать врага");
+        Console.WriteLine("Pudge начал заживо пожирать врага!");
+    }
+}
+class Terrorblade : Hero
+{ 
+    public static void Atribute()
+    {
+        Terrorblade terrorblade = new Terrorblade();
+        terrorblade.Agility = 22;
+        terrorblade.Strength = 16;
+        terrorblade.Intelligence = 19;
+        Console.WriteLine($"Сила: {terrorblade.Strength}  Ловкость: {terrorblade.Agility}  Интеллект: {terrorblade.Intelligence}");
+    }
+    public static void Reflection()
+    {
+        Console.WriteLine("Terrorblade создал тёмного неуязвимого двойника противника!");
+    }
+    public static void ConjureImage()
+    {
+        Console.WriteLine("Terrorblade создал копию самого себя");
+    }
+    public static void Metamorphosis()
+    {
+        Console.WriteLine("Terrorblade превратился в могущественного демона!");
+    }
+    public static void Sunder()
+    {
+        Console.WriteLine("Terrorblade поменялся жизнеными силами!");
+    }
+}
+
+class Medusa : Hero
+{
+    public static void Atribute()
+    {
+        Medusa medusa = new Medusa();
+        medusa.Agility = 22;
+        medusa.Strength = 17;
+        medusa.Intelligence = 23;
+        Console.WriteLine($"Сила: {medusa.Strength}  Ловкость: {medusa.Agility}  Интеллект: {medusa.Intelligence}");
+    }
+    public static void SplitShot()
+    {
+        Console.WriteLine("Medusa магически расщепляет свои стрелы, атакуя несколько целей!");
+    }
+    public static void MysticSnake()
+    {
+        Console.WriteLine("Medusa выпустила энергетическую змею!");
+    }
+    public static void ManaShield()
+    {
+        Console.WriteLine("Medusa создала энергетический щит!");
+    }
+    public static void StoneGaze()
+    {
+        Console.WriteLine("Medusa превращяет врагов в камень!");
     }
 }
 
